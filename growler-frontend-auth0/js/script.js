1570a634a0699bc04d3e7c1080d5fa57b606deea
Joseph Callaway <jeauxcal@gmail.com>
2016-10-20 10:26:16 -0500

    Added Auth0 to front end and created basic html file

---
+++ growler-frontend-auth0/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+  $('#btn-login').on('click', function (e) {
+    e.preventDefault();
+    lock.show();
+  });
+});
+
+
+var lock = new
+//1. Client ID, 2. Client Domain, 3. Oject of Attr
+  Auth0Lock('GoBNjyrd7W9Jg1HECE7nH82QUhjTsM2B', 'jeauxy.auth0.com', {
+    auth: {
+      params: {
+        scope: 'openid email'
+    }
+  }
+});
+
+lock.on('authenticated', function (authResult) {
+  console.log(authResult);
+  localStorage.setItem('idToken', authResult.idToken);
+  console.log('Logged In!');
+  // loadGrowls();
+});
+
+function logout() {
+    localStorage.removeItem('idToken')
+    window.location.href = '/';
+}
