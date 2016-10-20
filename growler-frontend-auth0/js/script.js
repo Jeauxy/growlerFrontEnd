ae27e28c42fa72c201e038c667d7ec51207bdf33
Joseph Callaway <jeauxcal@gmail.com>
2016-10-20 12:29:24 -0500

    Already commited on backend

--- growler-frontend-auth0/js/script.js
+++ growler-frontend-auth0/js/script.js
@@ -4,7 +4,25 @@ $(document).ready(function() {
     e.preventDefault();
     lock.show();
   });
+
+  $('#logout').on('click', function (e) {
+    e.preventDefault();
+    logout();
+  });
 });
+
+function showProfile() {
+  $('#btn-login').hide();
+  $('#user-info').show();
+  lock.getProfile(localStorage.getItem('idToken'), function (error, profile) {
+    if (error){
+      logout();
+    } else {
+      console.log('profile', profile);
+      $('#fullName').text(profile.given_name);
+    }
+  })
+}
 
 function addNewGrowl() {
   $('#new-growl-form').on('submit', function (e) {
@@ -16,10 +34,10 @@ function addNewGrowl() {
         'Authorization': 'Bearer ' + localStorage.getItem('idToken')
       },
       data: {
-        firstName: $('#student-name').val()
+        text: $('#growl-name').val()
       }
-    }).done(function (newStudent) {
-      loadStudent(newStudent)
+    }).done(function (newGrowl) {
+      loadGrowl(newGrowl)
       $('#growl-name').val('').focus()
     })
   })
@@ -42,7 +60,7 @@ function loadGrowl(growl) {
     var li = $('<li />')
     li.text(growl.text + ' ')
     li.data('id', growl._id);
-    if (growl.completed){
+    if (growl.text){
       li.addClass('done');
     }
     var deleteLink = $('<a />');
@@ -53,7 +71,7 @@ function loadGrowl(growl) {
     li.append(deleteLink)
 
     $('#growls').append(li);
-  }
+}
 
 var lock = new
 //1. Client ID, 2. Client Domain, 3. Oject of Attr
@@ -65,11 +83,75 @@ var lock = new
   }
 });
 
+function isLoggedIn() {
+  var token = localStorage.getItem('idToken')
+  var expired = isJwtValid(token);
+
+  if (token) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+// function isJwtValid(token) {
+//   var token = localStorage.getItem('idToken');
+//   if (!token){
+//     return false;
+//   }
+//   var payload = token.split('.')[1];
+//   payload = atob(payload);
+//   payload = JSON.parse(payload);
+//   exp = payload.exp
+//   console.log(payload);
+//   exp = exp * 1000;
+//   exp = new Date(exp);
+//   console.log(exp);
+//   current = new Date();
+//   console.log(current);
+//
+//   if (exp < current){
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function toggleGrowl(e) {
+//   console.log(toggleGrowl);
+//   e.preventDefault();
+//   var li = $(e.currentTarget)
+//   var url = 'http://localhost:3000/students/' + li.data('id')
+//   var newCompleted = !li.hasClass('done')
+//   $.ajax({
+//     url: url,
+//     method: 'PUT',
+//     data: {
+//       completed: newCompleted
+//     },
+//     headers: {
+//       'Authorization': 'Bearer ' + localStorage.getItem('idToken')
+//     }
+//   }).done(function () {
+//     li.toggleClass('done')
+//   })
+// }
+
+function logout() {
+  localStorage.removeItem('idToken')
+  window.location.href = '/';
+}
+
+
 lock.on('authenticated', function (authResult) {
   console.log(authResult);
   localStorage.setItem('idToken', authResult.idToken);
   console.log('Logged In!');
   loadGrowls();
+  showProfile();
+  addNewGrowl();
+  $('#welcome').hide();
 });
 
 function logout() {
